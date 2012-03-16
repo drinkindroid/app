f80a9a72616e4444b8ef4527a64d0913bb53db9a
Matt Hall <matt@docracy.com>
2012-03-15 21:55:04 -0400

    First commit with physics example and references to andengine projects.

---
+++ src/com/drinkindroid/DrinkinDroidMainActivity.java
@@ -0,0 +1,15 @@
+package com.drinkindroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrinkinDroidMainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
